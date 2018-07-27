c48b37bd14bd5320aef885a60305bd41cf66fbf2
Abigail Slovick <abby.slovick@gmail.com>
2018-07-27 15:57:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,23 @@
-# dog.rb
+# # dog.rb
+# class Dog
+#   def name
+#     @name
+#   end
+
+#   def name=(name)
+#     @name = name
+#   end
+
+#   def breed
+#     @breed
+#   end
+
+#   def breed=(breed)
+#     @breed = breed
+#   end
+# end
+
 class Dog
-  def name
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def breed
-    @breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-end
-
-# class dog
-#   attr_accessor :name, :breed
+  attr_accessor :name, :breed
   
-# end 
+end 
